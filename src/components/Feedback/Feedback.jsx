e275ea7db117e613d3ccb147e0b3f1ac9d38733f
Pallavi Soldey <pallavisolday12@gmail.com>
2022-08-14 12:49:22 +0530

    cutomise Bootstrap

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './feedback.css';
 import Heading from '../Heading';
 import yogaMat from '../../Images/yogaMat.jpg';
@@ -9,31 +9,118 @@ function Feedback() {
   const [ratingValue, setRatingValue] = useState(3);
   return (
     <section className='feedback'>
-        <Heading heading="Happy Customers"/>
-        <div className='feedback-container'>
-             <Card 
-                 img = {yogaMat}
-                 feedback="I am very happy after buying this ..."
-                 name = "Pallavi Soldey"
-                 city = "Akalatara"
-                 ratingValue = {ratingValue}
-             />
-               <Card 
-                 img = {yogaStrap}
-                 feedback="I am very happy after buying this ..."
-                 name = "Pallavi Soldey"
-                 city = "Bilaspur"
-                 ratingValue = {ratingValue}
-             />
-               <Card 
-                 img = {faceMask}
-                 feedback="I am very happy after buying this ..."
-                 name = "Pallavi Soldey"
-                 city = "Raipur"
-                 ratingValue = {ratingValue}
-             />
-              
+      <Heading heading="Happy Customers" />
+      <div className='quotes'>
+        <p><i class="fa fa-quote-left"></i>In tough times, It's harder and even more important to keep our customers happy<i class="fa fa-quote-right"></i></p>
+      </div>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active pointer"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1" class="pointer"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2" class="pointer"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div className='feedback-container'>
+              <Card
+                img={yogaMat}
+                feedback="I am very happy after buying this ..."
+                name="Pallavi Soldey"
+                city="Akalatara"
+                ratingValue={ratingValue}
+              />
+              <Card
+                img={yogaStrap}
+                feedback="I am very happy after buying this ..."
+                name="Pallavi Soldey"
+                city="Bilaspur"
+                ratingValue={ratingValue}
+              />
+              <Card
+                img={faceMask}
+                feedback="I am very happy after buying this ..."
+                name="Pallavi Soldey"
+                city="Raipur"
+                ratingValue={ratingValue}
+              />
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div className='feedback-container'>
+              <Card
+                img={yogaMat}
+                feedback="I am very happy after buying this ..."
+                name="Pallavi Soldey"
+                city="Akalatara"
+                ratingValue={ratingValue}
+              />
+              <Card
+                img={yogaStrap}
+                feedback="I am very happy after buying this ..."
+                name="Pallavi Soldey"
+                city="Bilaspur"
+                ratingValue={ratingValue}
+              />
+              <Card
+                img={faceMask}
+                feedback="I am very happy after buying this ..."
+                name="Pallavi Soldey"
+                city="Raipur"
+                ratingValue={ratingValue}
+              />
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div className='feedback-container'>
+              <Card
+                img={yogaMat}
+                feedback="I am very happy after buying this ..."
+                name="Pallavi Soldey"
+                city="Akalatara"
+                ratingValue={ratingValue}
+              />
+              <Card
+                img={yogaStrap}
+                feedback="I am very happy after buying this ..."
+                name="Pallavi Soldey"
+                city="Bilaspur"
+                ratingValue={ratingValue}
+              />
+              <Card
+                img={faceMask}
+                feedback="I am very happy after buying this ..."
+                name="Pallavi Soldey"
+                city="Raipur"
+                ratingValue={ratingValue}
+              />
+            </div>
+          </div>
         </div>
+      </div>
+      {/* <div className='feedback-container'>
+           <Card 
+               img = {yogaMat}
+               feedback="I am very happy after buying this ..."
+               name = "Pallavi Soldey"
+               city = "Akalatara"
+               ratingValue = {ratingValue}
+           />
+             <Card 
+               img = {yogaStrap}
+               feedback="I am very happy after buying this ..."
+               name = "Pallavi Soldey"
+               city = "Bilaspur"
+               ratingValue = {ratingValue}
+           />
+             <Card 
+               img = {faceMask}
+               feedback="I am very happy after buying this ..."
+               name = "Pallavi Soldey"
+               city = "Raipur"
+               ratingValue = {ratingValue}
+           />
+            
+      </div> */}
     </section>
   )
 }
